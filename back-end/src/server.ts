2cfe37a45df9808e76236d457b661d8fa169b74c
CristianoV <cristianoviieira@gmail.com>
2022-11-16 20:04:28 -0300

    feat: novas rotas adicionadas

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,20 +1,32 @@
-import app from './app';
-// require('express-async-errors');
+import { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
-import dotenv from 'dotenv';
+import dotenv = require('dotenv');
 dotenv.config();
 
+import app from './app';
 import Login from './Routes/Login.route';
 import Register from './Routes/Register.route';
+import Account from './Routes/Account.route';
+import Transaction from './Routes/Transaction.route';
 
 const port = process.env.API_PORT || 3000;
 
-app.get('/', (_request, response) => {
-  response.send('Hello World!');
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
 app.use('/login', Login);
 app.use('/register', Register);
+app.use('/account', Account);
+app.use('/transaction', Transaction);
 
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err) {
+    console.log(err);
+    
+    return res.status(403).json({ error: err.message });
+  }
+  next();
+});
 
 app.listen(port, () => console.log('ouvindo porta', port));
