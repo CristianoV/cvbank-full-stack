1195b7fb235e725bbdc1d6f94b42aa5b32201e1e
CristianoV <cristianoviieira@gmail.com>
2022-11-18 08:47:32 -0300

    feat: configurações de projeto

---
+++ web/tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
+      backgroundImage: {
+        app: "url('/effects.png')",
+      },
+      colors: {
+        ignite: {
+          500: '#129E57'
+        },
+        yellow: {
+          500: '#F7DD43',
+          700: '#E5CD3D'
+        },
+        gray: {
+          100: '#E1E1E6',
+          300: '#8D8D99',
+          600: '#323238',
+          800: '#202024',
+          900: '#121214',
+        },
+        primary: '#036B52',
+        secondary: '#2FC18C',
+        tertiary: '#421981',
+        quaternary: '#056CF9',
+        success: '#3CB371',
+        info: '#1E90FF',
+        warning: '#FFD700',
+        danger: '#DC143C',
+        light: '#F0F8FF',
+        dark: '#000000',
+      },
+    },
+  },
+  plugins: [],
+}
