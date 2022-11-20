2cfb2c78b9e898efd1641ab2dff037521cd24521
CristianoV <cristianoviieira@gmail.com>
2022-11-20 19:52:27 -0300

    feat(back): adiciona variaveis para ser utilizadas como mock

---
+++ back-end/src/tests/mocks/LoginMocks.ts
@@ -0,0 +1,29 @@
+export const login = {
+  username: 'CintiaS',
+  password: 'Abacaxi123',
+};
+
+export const usernameWrong = {
+  username: 'wrong',
+  password: 'Abacaxi123',
+};
+
+export const passwordWrong = {
+  username: 'CintiaS',
+  password: 'Wr0ngPassword',
+};
+
+export const smallUserName = {
+  username: 'Ci',
+  password: 'Abacaxi123',
+};
+
+export const smallPassword = {
+  username: 'CintiaS',
+  password: 'Ab',
+};
+
+export const alphaNumericPassword = {
+  username: 'CintiaS',
+  password: 'abacaxi123',
+};
