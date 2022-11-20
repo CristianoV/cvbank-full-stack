f5ae6151b6dfd900a2d1b84584f6a74f8e69a19e
CristianoV <cristianoviieira@gmail.com>
2022-11-20 19:47:36 -0300

    fix(back): configurando data do backend

--- back-end/src/database/migrations/20221115192415-Transactions.js
+++ back-end/src/database/migrations/20221115192415-Transactions.js
@@ -33,7 +33,7 @@ module.exports = {
           allowNull: false,
         },
         createdAt: {
-          type: Sequelize.DATE,
+          type: Sequelize.DATEONLY,
         },
       },
       {
