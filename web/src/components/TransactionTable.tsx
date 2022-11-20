e232666babb99d64e14db59c5f33725689120e00
CristianoV <cristianoviieira@gmail.com>
2022-11-20 20:01:10 -0300

    feat(front): adiciona validações visuais da pagina de login e register

---
+++ web/src/components/TransactionTable.tsx
@@ -0,0 +1,149 @@
+import { useRouter } from 'next/router';
+import { useEffect, useMemo, useState } from 'react';
+import { useAppContext } from '../context/AppContext';
+import { fetchFromApi } from '../lib/axios';
+import TransactionCells from '../components/TransactionCells';
+import Loading from './Loading';
+
+export default function TransactionTable() {
+  const [state, setState] = useAppContext() as any;
+  const [loading, setLoading] = useState(true);
+  const [date, setDate] = useState('');
+  const [filter, setFilter] = useState('');
+  const router = useRouter();
+
+  useEffect(() => {
+    const transactions = async () => {
+      const token = localStorage.getItem('token');
+      if (token) {
+        const response = await fetchFromApi.get('/transaction/all', {
+          headers: {
+            authorization: token,
+          },
+        });
+        const { data } = response;
+        setState({ ...state, transactions: data, newTransaction: false });
+        setLoading(false);
+        return null;
+      }
+      router.push('/login');
+    };
+    if (state.newTransaction === true) {
+      transactions();
+    }
+  }, [router, state, setState]);
+
+  useEffect(() => {
+    return () => {
+      if (state.newTransaction === false) {
+        setState({ ...state, newTransaction: true });
+      }
+    };
+  }, []);
+
+  const filteredTransactions = async (e) => {
+    e.preventDefault();
+    const token = localStorage.getItem('token');
+    if (token) {
+      const response = await fetchFromApi.post(
+        '/transaction/date',
+        { date, type: filter },
+        {
+          headers: {
+            Authorization: token,
+          },
+        }
+      );
+      const { data } = response;
+      console.log(data);
+
+      setState({ ...state, transactions: data });
+      setLoading(false);
+      return null;
+    }
+    router.push('/login');
+  };
+
+  return (
+    <div className='flex flex-col justify-center pt-16'>
+      <form
+        onSubmit={filteredTransactions}
+        className='flex justify-center items-end m-6 gap-4'
+      >
+        <label htmlFor='data' className='flex flex-col items-center'>
+          Selecione o dia
+          <input
+            className='border border-[#001813] rounded-md p-1 w-60 h-11 shadow-md'
+            type='date'
+            pattern='[0-9]{4}-[0-9]{2}-[0-9]{2}'
+            required
+            name=''
+            id='data'
+            onChange={(e) => {
+              setDate(e.target.value);
+            }}
+          />
+        </label>
+        <label htmlFor='' className='flex flex-col items-center'>
+          Tipo de Transação
+          <select
+            name=''
+            id=''
+            className='border border-[#001813] rounded-md p-1 w-60 h-11 shadow-md'
+            onChange={(e) => {
+              setFilter(e.target.value);
+            }}
+          >
+            <option value='1'>Selecione uma opção</option>
+            <option value='1'>Todas</option>
+            <option value='debit'>Transferido</option>
+            <option value='credit'>Depositado</option>
+          </select>
+        </label>
+        <button
+          className='bg-bank-primary rounded-lg w-60 h-11 text-white
+                  disabled:opacity-50 disabled:cursor-not-allowed hover:bg-bank-secondary'
+          type='submit'
+        >
+          Pesquisar
+        </button>
+      </form>
+
+      <table border={1} className='border border-black mx-auto mb-28 w-6/12'>
+        <thead>
+          <tr>
+            <th>VALOR</th>
+            <th>Creditado</th>
+            <th>DEBITADO</th>
+            <th>DATA</th>
+          </tr>
+        </thead>
+        <tbody>
+          {!loading &&
+            state.transactions.map(
+              (
+                { createdAt, value, id, creditedUser, debitedUser }: any,
+                index
+              ) => {
+                return (
+                  <TransactionCells
+                    createdAt={createdAt}
+                    value={value}
+                    creditedAccountId={creditedUser}
+                    debitedAccountId={debitedUser}
+                    id={id}
+                    key={index}
+                  />
+                );
+              }
+            )}
+        </tbody>
+      </table>
+      {loading && (
+        <div className='mx-auto mb-28'>
+          <Loading size={6} />
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ web/src/components/ValidatePassword.tsx
@@ -0,0 +1,18 @@
+export default function ValidatePassword({ password }) {
+  return (
+    <div className='text-xs flex gap-2'>
+      <h1 className={password.length > 8 ? 'text-green-700' : 'text-black'}>
+        8 caracteres
+      </h1>
+      <h1 className={password.match(/[A-Z]/) ? 'text-green-700' : 'text-black'}>
+        1 letra maiúscula
+      </h1>
+      <h1 className={password.match(/[a-z]/) ? 'text-green-700' : 'text-black'}>
+        1 letra minúscula
+      </h1>
+      <h1 className={password.match(/[0-9]/) ? 'text-green-700' : 'text-black'}>
+        1 número
+      </h1>
+    </div>
+  );
+}
