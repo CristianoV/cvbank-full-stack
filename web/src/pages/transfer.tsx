26158baba246602ebcf65747527a15b476f502b8
CristianoV <cristianoviieira@gmail.com>
2022-11-20 19:56:15 -0300

    feat(front): adiciona pagina de transferencia

---
+++ web/src/pages/transfer.tsx
@@ -0,0 +1,11 @@
+import InfoAcount from '../components/InfoAcount';
+import NewTransaction from '../components/NewTransaction';
+
+export default function Account() {
+  return (
+    <div>
+      <InfoAcount />
+      <NewTransaction />
+    </div>
+  );
+}
