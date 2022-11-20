e6284ce07c31d23527419f183f81e78b583f9431
CristianoV <cristianoviieira@gmail.com>
2022-11-20 19:49:25 -0300

    feat(back): adicionando variavel de ambiente no database

--- back-end/src/database/config/database.ts
+++ back-end/src/database/config/database.ts
@@ -4,7 +4,7 @@ import { Options } from 'sequelize';
 const config: Options = {
   username: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASS || '1234',
-  database: 'database_development',
+  database: process.env.NODE_ENV || 'database_development',
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 5432,
   dialect: 'postgres',
