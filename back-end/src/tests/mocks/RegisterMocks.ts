ce596ac46ebc93d614489fd4ff42a10952481bf1
CristianoV <cristianoviieira@gmail.com>
2022-11-20 23:37:13 -0300

    feat(back): mocks para os testes de register

---
+++ back-end/src/tests/mocks/RegisterMocks.ts
@@ -0,0 +1,24 @@
+export const register = {
+  username: 'AntonioS',
+  password: 'Abacaxi123',
+};
+
+export const passwordWrong = {
+  username: 'AntonioS',
+  password: 'Wr0ngPassword',
+};
+
+export const smallUserName = {
+  username: 'Ci',
+  password: 'Abacaxi123',
+};
+
+export const smallPassword = {
+  username: 'AntonioS',
+  password: 'Ab',
+};
+
+export const alphaNumericPassword = {
+  username: 'AntonioS',
+  password: 'abacaxi123',
+};
