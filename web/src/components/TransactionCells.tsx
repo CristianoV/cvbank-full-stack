1c27e2e4ff486d1cfd46db75d4ab74fa3cda5115
CristianoV <cristianoviieira@gmail.com>
2022-11-22 18:20:02 -0300

    feat(front): componente TransactionCells refatorado

--- web/src/components/TransactionCells.tsx
+++ web/src/components/TransactionCells.tsx
@@ -1,38 +1,42 @@
 import PropTypes from 'prop-types';
 import { useAppContext } from '../context/AppContext';
 import moment from 'moment';
+import { IContext } from '../interfaces/IData/IContext';
+import { ITransaction } from '../interfaces/IData/ITransaction';
 
 function TransactionCells({
   createdAt,
   value,
-  creditedAccountId,
-  debitedAccountId,
+  creditedUser,
+  debitedUser,
   id,
-}) {
-  const [state, setState] = useAppContext() as any;
+}: ITransaction) {
+  const [state, setState] = useAppContext() as unknown as [
+    IContext,
+    (state: IContext) => void
+  ];
   const priceFormat = new Intl.NumberFormat('pt-BR', {
     style: 'currency',
     currency: 'BRL',
   });
-  
-  // const date = moment(createdAt).format('LL');
+
   const date = moment(createdAt).format('DD/MM/YYYY');
 
   return (
     <tr key={id}>
       <td
         className={`text-center ${
-          state.username === debitedAccountId.username
+          state.username === debitedUser.username
             ? 'text-danger'
             : 'text-success'
         }`}
       >
-        {`${state.username === debitedAccountId.username ? '-' : '+'} ${priceFormat.format(
-          value / 100
-          )}`}
+        {`${
+          state.username === debitedUser.username ? '-' : '+'
+        } ${priceFormat.format(value / 100)}`}
       </td>
-      <td className='text-center'>{creditedAccountId.username}</td>
-      <td className='text-center'>{debitedAccountId.username}</td>
+      <td className='text-center'>{creditedUser.username}</td>
+      <td className='text-center'>{debitedUser.username}</td>
       <td className='text-center'>{date}</td>
     </tr>
   );
@@ -40,12 +44,6 @@ function TransactionCells({
 
 TransactionCells.propTypes = {
   id: PropTypes.number.isRequired,
-  creditedAccountId: PropTypes.shape({
-    username: PropTypes.string.isRequired,
-  }).isRequired,
-  debitedAccountId: PropTypes.shape({
-    username: PropTypes.string.isRequired,
-  }).isRequired,
   value: PropTypes.number.isRequired,
   createdAt: PropTypes.string.isRequired,
 };
