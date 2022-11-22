3086731166cf45c0371e0e07c200b4c22d9160f5
CristianoV <cristianoviieira@gmail.com>
2022-11-22 14:34:18 -0300

    feat(back): adiciona testes da rota de registro

--- back-end/src/tests/Register.test.ts
+++ back-end/src/tests/Register.test.ts
@@ -1,19 +1,49 @@
 import {
+  newAccount,
+  newUsser,
   register,
   smallUserName,
   smallPassword,
   alphaNumericPassword,
 } from './mocks/RegisterMocks';
 import * as chai from 'chai';
+// @ts-ignore
 import chaiHttp = require('chai-http');
+import 'mocha';
+import * as Sinon from 'sinon';
+import { app } from '../app';
+
+import User from '../database/models/user';
+import Account from '../database/models/account';
+
+import { afterEach } from 'mocha';
 
 chai.use(chaiHttp);
 
-describe('Testando rotas', () => {
-  describe('register', () => {
+describe('Testando rota de Registro', () => {
+  describe('Sucesso', () => {
+    afterEach(() => {
+      Sinon.restore();
+    });
+    it('Testando register com tudo correto', async () => {
+      Sinon.stub(User, 'findOne').returns(Promise.resolve(null));
+      Sinon.stub(User, 'create').returns(newUsser as any);
+      Sinon.stub(Account, 'create').returns(newAccount as any);
+      const response = await chai.request(app).post('/register').send(register);
+
+      chai.expect(response.status).to.be.equal(201);
+      chai.expect(response.status).to.equal(201);
+      chai.expect(response.body).to.be.key('token');
+    });
+  });
+  describe('Falha', () => {
+    afterEach(() => {
+      Sinon.restore();
+    });
     it('Testando register com username errado menor que 3 caracteres', async () => {
+      Sinon.stub(User, 'findOne').returns(Promise.resolve(null));
       const response = await chai
-        .request('http://localhost:3333')
+        .request(app)
         .post('/register')
         .send(smallUserName);
 
@@ -25,8 +55,9 @@ describe('Testando rotas', () => {
       });
     });
     it('Testando register com senha errada menor que 8 caracteres', async () => {
+      Sinon.stub(User, 'findOne').returns(Promise.resolve(null));
       const response = await chai
-        .request('http://localhost:3333')
+        .request(app)
         .post('/register')
         .send(smallPassword);
 
@@ -38,8 +69,9 @@ describe('Testando rotas', () => {
       });
     });
     it('Testando register com senha faltando validações', async () => {
+      Sinon.stub(User, 'findOne').returns(Promise.resolve(null));
       const response = await chai
-        .request('http://localhost:3333')
+        .request(app)
         .post('/register')
         .send(alphaNumericPassword);
 
@@ -49,16 +81,6 @@ describe('Testando rotas', () => {
       chai.expect(response.body).to.be.deep.equal({
         error: 'model must be alphanumeric',
       });
-    });
-    it('Testando register com tudo correto', async () => {
-      const response = await chai
-        .request('http://localhost:3333')
-        .post('/register')
-        .send(register);
-
-      chai.expect(response.status).to.be.equal(201);
-      chai.expect(response.status).to.equal(201);
-      chai.expect(response.body).to.be.key('token');
     });
   });
 });
