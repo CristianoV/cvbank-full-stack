028b205d71e9b8d565632577924ce98175e9e50b
CristianoV <cristianoviieira@gmail.com>
2023-02-22 19:09:39 -0300

    style: alteração na estilização

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -5,11 +5,20 @@ export default function Document() {
     <Html>
       <Head>
         {/* <link rel='icon' href='/favicon.ico' /> */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin=''
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap'
+          rel='stylesheet'
+        />
       </Head>
-      <body className='bg-bank-quaternary bg-app bg-no-repeat bg-cover'>
+      {/* <body className='bg-bank-quaternary bg-app bg-no-repeat bg-cover'>
+       */}
+      <body className='bg-bank-quaternary'>
         <Main />
         <NextScript />
       </body>
