f44412bf2fa617d5e62c0ed0b3542160473c4a4b
CristianoV <cristianoviieira@gmail.com>
2023-02-23 21:19:43 -0300

    feat(front): cria nova pagina de perfil

---
+++ web/src/pages/profile.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import InfoAccount from '../components/InfoAccount';
+import LeftNavBar from '../components/LeftNavBar';
+
+export default function Profile() {
+  return (
+    <>
+      <Head>
+        <title>Banco NG</title>
+      </Head>
+      <div>
+        <InfoAccount />
+        <div className='flex max-w-screen-xl mx-auto'>
+          <LeftNavBar />
+          <div className='mx-auto'></div>
+        </div>
+      </div>
+    </>
+  );
+}
