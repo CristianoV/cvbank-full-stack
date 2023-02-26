c33e69f62ff6c5727553d54f8247397363eb0b95
CristianoV <cristianoviieira@gmail.com>
2023-02-26 20:58:57 -0300

    feat(front): adiciona novo estilo a pagina de login e registro

--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -4,7 +4,10 @@ import { fetchFromApi } from '../lib/axios';
 import { useAppContext } from '../context/AppContext';
 import ValidatePassword from '../components/ValidatePassword';
 import ValidateUsername from '../components/ValidateUsername';
-import Loading from '../components/Loading';
+import imageLogin from '../../public/loginImg.jpg';
+import Image from 'next/image';
+import Link from 'next/link';
+import Head from 'next/head';
 
 export default function Login() {
   const [username, setUsername] = useState('');
@@ -39,7 +42,7 @@ export default function Login() {
         router.push('/account');
       }
 
-      setError(data.error); 
+      setError(data.error);
       setPassword('');
       setUsername('');
     } catch (error: any) {
@@ -53,84 +56,124 @@ export default function Login() {
   };
 
   return (
-    <div>
-      <form
-        onSubmit={userLogin}
-        className='flex justify-center flex-col items-center gap-3 bg-[#E5E5E5]
-      w-96 h-96 m-auto mt-20 place-content-center font-roboto shadow-lg rounded-lg'
-      >
-        <label htmlFor='username' className='flex flex-col'>
-          Apelido
-          <input
-            className='border border-[#001813] rounded-md p-1 w-80 h-11 shadow-md'
-            type='input'
-            placeholder='Seu apelido'
-            required
-            id='username'
-            value={username}
-            onChange={({ target }) => setUsername(target.value)}
+    <>
+      <Head>
+        <title>Banco NG</title>
+      </Head>
+      <div className='flex flex-col bg-gray-100'>
+        <header className='bg-bank-primary text-bank-quaternary'>
+          <div className='flex justify-center items-center h-16 w-full max-w-screen-xl'>
+            <Link href='/'>
+              <h1 className='text-4xl font-bold transition duration-500 ease-in-out animate-pulse'>
+                NGBANCO<span className='text-violet-500'>.</span>
+              </h1>
+            </Link>
+          </div>
+        </header>
+        <div className='flex'>
+          <Image
+            src={imageLogin}
+            alt='Imagem de login'
+            className='object-cover w-7/12 h-screen'
+            quality={50}
+            placeholder='blur'
+            blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mN0vQgAAWEBGHsgcxcAAAAASUVORK5CYII='
           />
-          <ValidateUsername username={username} />
-        </label>
-        <label htmlFor='password' className='flex flex-col'>
-          Senha
-          <input
-            className='border border-[#001813] rounded-md p-1 w-80 h-11 shadow-md'
-            type={seePassword ? 'text' : 'password'}
-            placeholder='*******'
-            required
-            id='password'
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
-          />
-          <label htmlFor='ps' className='flex gap-2'>
-            <input
-              type='checkbox'
-              name=''
-              checked={seePassword}
-              id='ps'
-              onChange={() => setSeePassword(!seePassword)}
-            />
-            Mostrar senha
-          </label>
-          <ValidatePassword password={password} />
-        </label>
-        {error && (
-          <h6 className='text-danger text-sm font-bold text-center'>
-            O apelido utilizado ou a senha inseridas estão incorretos. Tente
-            novamente, por favor.
-          </h6>
-        )}
-    <button
-      className={`bg-bank-primary rounded-lg w-80 h-11 text-white 
-        ${loading ? 'opacity-50 cursor-not-allowed' : 'hover:bg-bank-secondary'}`}
-      type='submit'
-      disabled={loading}
-    >
-      {loading ? (
-        <div className="flex items-center justify-center">
-          <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
-            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm12 0a8 8 0 100-16v3a5 5 0 010 10v3z"
-            />
-          </svg>
-          <span>Carregando...</span>
+          <form
+            onSubmit={userLogin}
+            className='flex justify-center flex-col items-center gap-3 bg-[#E5E5E5]
+      m-auto p-5 mt-20 place-content-center font-roboto shadow-lg rounded-lg'
+          >
+            <h1 className='text-2xl font-bold text-bank-primary'>
+              Inicie sessão
+            </h1>
+            <p>Adicione um usuario e uma senha!</p>
+            <label htmlFor='username' className='flex flex-col'>
+              Apelido
+              <input
+                className='border border-[#001813] rounded-md p-1 w-80 h-11 shadow-md'
+                type='input'
+                placeholder='Seu apelido'
+                required
+                id='username'
+                value={username}
+                onChange={({ target }) => setUsername(target.value)}
+              />
+              <ValidateUsername username={username} />
+            </label>
+            <label htmlFor='password' className='flex flex-col'>
+              Senha
+              <input
+                className='border border-[#001813] rounded-md p-1 w-80 h-11 shadow-md'
+                type={seePassword ? 'text' : 'password'}
+                placeholder='*******'
+                required
+                id='password'
+                value={password}
+                onChange={({ target }) => setPassword(target.value)}
+              />
+              <label htmlFor='ps' className='flex gap-2'>
+                <input
+                  type='checkbox'
+                  name=''
+                  checked={seePassword}
+                  id='ps'
+                  onChange={() => setSeePassword(!seePassword)}
+                />
+                Mostrar senha
+              </label>
+              <ValidatePassword password={password} />
+            </label>
+            {error && (
+              <h6 className='text-danger text-sm font-bold text-center'>
+                O apelido utilizado ou a senha inseridas estão incorretos. Tente
+                novamente, por favor.
+              </h6>
+            )}
+            <button
+              className={`bg-bank-primary rounded-lg w-80 h-11 text-white 
+        ${
+          loading ? 'opacity-50 cursor-not-allowed' : 'hover:bg-bank-secondary'
+        }`}
+              type='submit'
+              disabled={loading}
+            >
+              {loading ? (
+                <div className='flex items-center justify-center'>
+                  <svg
+                    className='animate-spin h-5 w-5 mr-3'
+                    viewBox='0 0 24 24'
+                  >
+                    <circle
+                      className='opacity-25'
+                      cx='12'
+                      cy='12'
+                      r='10'
+                      stroke='currentColor'
+                      strokeWidth='4'
+                    />
+                    <path
+                      className='opacity-75'
+                      fill='currentColor'
+                      d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm12 0a8 8 0 100-16v3a5 5 0 010 10v3z'
+                    />
+                  </svg>
+                  <span>Carregando...</span>
+                </div>
+              ) : (
+                <span>Entrar</span>
+              )}
+            </button>
+            <button
+              className='border-2 border-bank-primary rounded-lg w-80 h-11 text-bank-primary'
+              type='button'
+              onClick={() => router.push('/register')}
+            >
+              Ainda não tenho uma conta
+            </button>
+          </form>
         </div>
-      ) : (
-        <span>LOGIN</span>
-      )}
-    </button>
-        <button
-          className='border-2 border-bank-primary rounded-lg w-80 h-11 text-bank-primary'
-          type='button'
-          onClick={() => router.push('/register')}
-        >
-          Ainda não tenho uma conta
-        </button>
-      </form>
-    </div>
+      </div>
+    </>
   );
 }
