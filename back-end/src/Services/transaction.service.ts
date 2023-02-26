b0fa8f4e5c6380840e6a8abcc3939d5352c91965
CristianoV <cristianoviieira@gmail.com>
2023-02-26 18:18:17 -0300

    feat(back): adiciona função no service para transferência por pix

--- back-end/src/Services/transaction.service.ts
+++ back-end/src/Services/transaction.service.ts
@@ -2,54 +2,91 @@ import Transaction from '../database/models/transaction';
 import Account from '../database/models/account';
 import JwtSecret from '../utils/JwtService';
 import { Op } from 'sequelize';
-import { ITransactionData } from '../Interfaces/IData/ITransactionData';
-import User from '../database/models/user';
+import {
+  ITransactionData,
+  ITransactionPixData,
+  ITransactionTransferData,
+} from '../Interfaces/IData/ITransactionData';
 import { ITransactionService } from '../Interfaces/IService/ITransactionService';
 import { IFilterData } from '../Interfaces/IData/IFilterData';
 
 export default class TransactionService implements ITransactionService {
-  public async newTransaction({
-    creditedAccountId,
-    authorization,
+  public async executeTransaction({
+    creditedAccount,
+    debitedAccount,
     value,
   }: ITransactionData) {
-    const { id } = JwtSecret.verify(authorization) as { id: number };
-  
-    const creditedAccount = await Account.findOne({
-      include: [
-        {
-          model: User,
-          as: 'user',
-          where: { id: creditedAccountId },
-        },
-      ],
-    });
-  
-    if (!creditedAccount) {
-      throw new Error('Credited account not found');
-    }
-  
-    const debitedAccount = await Account.findByPk(id);
-  
-    if (!debitedAccount) {
-      throw new Error('Debited account not found');
-    }
-  
     const transaction = await Transaction.create({
       creditedAccountId: creditedAccount.id,
       debitedAccountId: debitedAccount.id,
       value,
     });
-  
+
     const newDebitedBalance = Number(debitedAccount.balance) - Number(value);
     const newCreditedBalance = Number(creditedAccount.balance) + Number(value);
-  
+
     await Promise.all([
       debitedAccount.update({ balance: newDebitedBalance }),
       creditedAccount.update({ balance: newCreditedBalance }),
     ]);
-  
+
     return transaction;
+  }
+
+  public async transferTransaction({
+    creditedAccountId,
+    authorization,
+    value,
+  }: ITransactionTransferData) {
+    const { id } = JwtSecret.verify(authorization) as { id: number };
+
+    const creditedAccount = await Account.findByPk(creditedAccountId);
+
+    if (!creditedAccount) {
+      throw new Error('Credited account not found');
+    }
+
+    const debitedAccount = await Account.findByPk(id);
+
+    if (!debitedAccount) {
+      throw new Error('Debited account not found');
+    }
+
+    return this.executeTransaction({
+      creditedAccount,
+      debitedAccount,
+      value,
+    });
+  }
+
+  public async pixTransaction({
+    pixKey,
+    authorization,
+    value,
+  }: ITransactionPixData) {
+    const { id } = JwtSecret.verify(authorization) as { id: number };
+
+    const creditedAccount = await Account.findOne({
+      where: {
+        pixKey,
+      },
+    });
+
+    if (!creditedAccount) {
+      throw new Error('Credited account not found');
+    }
+
+    const debitedAccount = await Account.findByPk(id);
+
+    if (!debitedAccount) {
+      throw new Error('Debited account not found');
+    }
+
+    return this.executeTransaction({
+      creditedAccount,
+      debitedAccount,
+      value,
+    });
   }
 
   public async getTransactions(authorization: string) {
