0964519f6ca088abf90a13a00645ef3f9a87eaeb
CristianoV <cristianoviieira@gmail.com>
2023-02-27 13:41:26 -0300

    feat(front): adiciona component de saldo

--- web/src/components/Balance.tsx
+++ web/src/components/Balance.tsx
@@ -1,6 +1,5 @@
 import { useAppContext } from '../context/AppContext';
 import { BsFillEyeFill, BsFillEyeSlashFill } from 'react-icons/bs';
-import { IoStorefrontOutline } from 'react-icons/io5';
 import { useState } from 'react';
 
 export default function Balance() {
@@ -12,33 +11,20 @@ export default function Balance() {
     currency: 'BRL',
   });
   return (
-    <div className='bg-white rounded p-3 m-2 w-6/12 h-72 text-2xl'>
-      <div className='flex justify-between'>
-        <h2>Saldo</h2>
-        <button onClick={() => setShow(!show)}>
-          {show === true ? (
-            <BsFillEyeFill size={22} />
-          ) : (
-            <BsFillEyeSlashFill size={22} />
-          )}
-        </button>
-      </div>
-      <div className='flex flex-col h-5/6 justify-between'>
-        <h2 className='mr-2 font-bold mt-3'>
+    <div className='bg-white rounded p-3 m-2 text-2xl flex justify-between w-full'>
+      <p className='mr-2 font-bold mt-3'>
+        Saldo:
+        <span className={`mt-4 ml-3 font-bold`}>
           {show === true ? priceFormat.format(state.balance / 100) : 'R$ ****'}
-        </h2>
-        <div>
-          <h1>Vendas a receber</h1>
-          <p className='flex items-center gap-3 font-bold'>
-            <IoStorefrontOutline size={22} />{' '}
-            {show === true ? 'R$ 0,00' : 'R$ ****'}
-          </p>
-        </div>
-        <div>
-          <h1>Bloqueado</h1>
-          <p className='font-bold'>{show === true ? 'R$ 0,00' : 'R$ ****'}</p>
-        </div>
-      </div>
+        </span>
+      </p>
+      <button onClick={() => setShow(!show)}>
+        {show === true ? (
+          <BsFillEyeFill size={22} />
+        ) : (
+          <BsFillEyeSlashFill size={22} />
+        )}
+      </button>
     </div>
   );
 }
