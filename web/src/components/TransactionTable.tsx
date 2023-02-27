0a5e507f9a9fe038a71b877b1662ba22900ded43
CristianoV <cristianoviieira@gmail.com>
2023-02-27 09:47:15 -0300

    feat(front): adiciona loading no botão de pesquisar

--- web/src/components/TransactionTable.tsx
+++ web/src/components/TransactionTable.tsx
@@ -49,24 +49,30 @@ export default function TransactionTable() {
 
   const filteredTransactions = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
-    const token = localStorage.getItem('token');
-    if (token) {
-      const response = await fetchFromApi.post(
-        '/transaction/filter',
-        { date, type: filter },
-        {
-          headers: {
-            Authorization: token,
-          },
-        }
-      );
-      const { data } = response;
+    try {
+      const token = localStorage.getItem('token');
+      setLoading(true);
+      if (token) {
+        const response = await fetchFromApi.post(
+          '/transaction/filter',
+          { date, type: filter },
+          {
+            headers: {
+              Authorization: token,
+            },
+          }
+        );
+        const { data } = response;
 
-      setState({ ...state, transactions: data });
+        setState({ ...state, transactions: data });
+        setLoading(false);
+        return null;
+      }
+    } catch (error: any) {
+      router.push('/login');
+    } finally {
       setLoading(false);
-      return null;
     }
-    router.push('/login');
   };
 
   return (
@@ -78,7 +84,7 @@ export default function TransactionTable() {
         <label htmlFor='data' className='flex flex-col items-center w-3/12'>
           Selecione o dia
           <input
-            className='border border-[#001813] w-full rounded-md p-1 h-11 shadow-md'
+            className='border border-[#001813] w-full rounded-md p-1 h-11 shadow-md cursor-pointer'
             type='date'
             pattern='[0-9]{4}-[0-9]{2}-[0-9]{2}'
             required
@@ -89,12 +95,12 @@ export default function TransactionTable() {
             }}
           />
         </label>
-        <label htmlFor='' className='flex flex-col items-center w-3/12'>
+        <label htmlFor='' className='flex flex-col items-center w-3/12 '>
           Tipo de Transação
           <select
             name=''
             id=''
-            className='border border-[#001813] w-full rounded-md p-1 h-11 shadow-md'
+            className='border border-[#001813] w-full rounded-md p-1 h-11 shadow-md cursor-pointer'
             onChange={(e) => {
               setFilter(e.target.value);
             }}
@@ -106,11 +112,37 @@ export default function TransactionTable() {
           </select>
         </label>
         <button
-          className='bg-bank-primary rounded-lg h-11 text-white w-3/12
-                  disabled:opacity-50 disabled:cursor-not-allowed hover:bg-bank-secondary'
+          className={`bg-bank-primary rounded-lg  w-3/12 h-11 text-white 
+        ${
+          loading
+            ? 'opacity-50 cursor-not-allowed'
+            : 'hover:bg-bank-secondary hover:opacity-95 transition-all duration-300 ease-in-out'
+        }`}
           type='submit'
+          disabled={loading}
         >
-          Pesquisar
+          {loading ? (
+            <div className='flex items-center justify-center'>
+              <svg className='animate-spin h-5 w-5 mr-3' viewBox='0 0 24 24'>
+                <circle
+                  className='opacity-25'
+                  cx='12'
+                  cy='12'
+                  r='10'
+                  stroke='currentColor'
+                  strokeWidth='4'
+                />
+                <path
+                  className='opacity-75'
+                  fill='currentColor'
+                  d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm12 0a8 8 0 100-16v3a5 5 0 010 10v3z'
+                />
+              </svg>
+              <span>Carregando...</span>
+            </div>
+          ) : (
+            <span>Pesquisar</span>
+          )}
         </button>
       </form>
       <div className='overflow-x-auto  w-full  h-50'>
