648f444dbd35bd721e9c00b12c518935655bfab6
CristianoV <cristianoviieira@gmail.com>
2023-03-01 17:16:47 -0300

    feat(back): adiciona boletos no banco de dados

---
+++ back-end/src/database/models/boleto.ts
@@ -0,0 +1,52 @@
+import { INTEGER, Model, DATEONLY, ENUM, BIGINT, STRING } from 'sequelize';
+import db from '.';
+import User from './user';
+
+class Boletos extends Model {
+  public id!: number;
+  public accountId!: number;
+  public value!: number;
+  public status!: string;
+  public createdAt!: Date;
+}
+
+Boletos.init(
+  {
+    id: {
+      type: BIGINT,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    boletoId: {
+      type: STRING,
+      allowNull: false,
+    },
+    accountId: {
+      type: INTEGER,
+      allowNull: false,
+      references: { model: 'Accounts', key: 'id' },
+    },
+    value: {
+      type: INTEGER,
+    },
+    status: {
+      type: ENUM('Pendente', 'Pago', 'Cancelado'),
+    },
+    createdAt: {
+      type: DATEONLY,
+    },
+  },
+  {
+    sequelize: db,
+    modelName: 'Boletos',
+    updatedAt: false,
+  }
+);
+
+Boletos.belongsTo(User, {
+  as: 'user',
+  foreignKey: 'accountId',
+});
+
+export default Boletos;
