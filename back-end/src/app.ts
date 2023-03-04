17525b4b9f9da028c312b1c40be6c35666b27842
CristianoV <cristianoviieira@gmail.com>
2023-03-04 17:03:03 -0300

    feat(back): adiciona swaggerUi ao projeto

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,4 +1,5 @@
 import express = require('express');
+import swaggerUi = require('swagger-ui-express');
 import 'express-async-errors';
 import Login from './Routes/Login.route';
 import Register from './Routes/Register.route';
@@ -8,6 +9,7 @@ import Boleto from './Routes/Boleto.route';
 import NotFound from './Routes/NotFound';
 import cors = require('cors');
 import { Error } from 'sequelize';
+const swaggerDocument = require('../swagger.json');
 
 class App {
   public app: express.Express;
@@ -18,24 +20,36 @@ class App {
     this.config();
   }
 
-  private config():void {
+  private config(): void {
     this.app.use(cors());
     this.app.use(express.json());
+    this.app.use(
+      '/api-docs',
+      swaggerUi.serve,
+      swaggerUi.setup(swaggerDocument)
+    );
     this.app.use('/login', Login);
     this.app.use('/register', Register);
     this.app.use('/account', Account);
     this.app.use('/transaction', Transaction);
     this.app.use('/boleto', Boleto);
-    this.app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
-      if (err) {
-        return res.status(400).json({ error: err.message });
+    this.app.use(
+      (
+        err: Error,
+        _req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+      ) => {
+        if (err) {
+          return res.status(400).json({ error: err.message });
+        }
+        next();
       }
-      next();
-    });
-    this.app.use(NotFound)
+    );
+    this.app.use(NotFound);
   }
 
-  public start(PORT: string | number):void {
+  public start(PORT: string | number): void {
     this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
   }
 }
